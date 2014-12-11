4f4a7a14f96762ba95cd67534b4ed13542ba2853
unknown <A00226316@W301-A41.studentnet.ait.ie>
2014-12-11 21:07:59 +0000

    commit 11 Dec 2015

---
+++ Practice/src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+	public void ifTest(boolean flag){
+		if(flag){
+			if(flag){
+				System.out.println("True Fa." +
+						"lse");
+			}else{
+				System.out.println("True True");	
+			}
+		}else{
+			System.out.println("False False");	
+		}
+		
+	}
+}
